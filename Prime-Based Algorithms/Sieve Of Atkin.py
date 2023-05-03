aae01e387427fb54e70d54e491372847c4532b30
LiamP921 <125910815+LiamP921@users.noreply.github.com>
2023-05-03 22:05:02 +0100

    Update Sieve Of Atkin.py

--- Prime-Based Algorithms/Sieve Of Atkin.py
+++ Prime-Based Algorithms/Sieve Of Atkin.py
@@ -1,46 +1,45 @@
-import numpy as np
-import time
-
 def sieveOfAtkin(limit):
     if limit < 2:
-        return []
+        return
 
-    sieve = np.zeros(limit + 1, dtype=bool)
-    primes = [2, 3]
-    sqrt_limit = int(np.sqrt(limit)) + 1
+    """ all numbers within the limit are deemed composites, which are sieved in three layers: """
+    sieve = [False] * (limit + 1)
+    sieve[2], sieve[3] = True, True
+    sqrt_limit = int(limit ** 0.5) + 1
+    squares = [i * i for i in range(1, sqrt_limit)]
 
     for x in range(1, sqrt_limit):
-        for y in range(1, sqrt_limit):
-            n = 4 * x**2 + y**2
+        x_square = squares[x - 1]
+        for y_square in squares:
+            """ a) 'n = (4 * x * x) + (y * y)' has an odd no. of solutions (i.e., there's an odd number of distinct pairs '(x, y)' that satisfy 
+            'and n % 12 = 1 or n % 12 = 5'). """
+            n = 4 * x_square + y_square
             if n <= limit and (n % 12 == 1 or n % 12 == 5):
                 sieve[n] = not sieve[n]
 
-            n = 3 * x**2 + y**2
+            """ b) 'n = (3 * x * x) + (y * y)' has an odd no. of solutions 'and n % 12 = 7'. """
+            n = 3 * x_square + y_square
             if n <= limit and n % 12 == 7:
                 sieve[n] = not sieve[n]
 
-            n = 3 * x**2 - y**2
-            if x > y and n <= limit and n % 12 == 11:
-                sieve[n] = not sieve[n]
+            """ c) 'n = (3 * x * x) - (y * y)' has an odd no. of solutions 'x > y and n % 12 = 11'. """
+            if x > y_square ** 0.5:
+                n = 3 * x_square - y_square
+                if n <= limit and n % 12 == 11:
+                    sieve[n] = not sieve[n]
 
-    for n in range(5, sqrt_limit):
+    """ 'yield' returns a generator object to the function caller, rather than a value. """
+    yield 2
+    yield 3
+
+    """ mark all mulitples of squares as non-prime. """
+    for n in range(5, limit + 1):
         if sieve[n]:
-            primes.append(n)
-            sieve[n**2::n**2] = False
-
-    for n in range(sqrt_limit, limit+1):
-        if sieve[n]:
-            primes.append(n)
-
-    return primes
-
-def nthPrime(n):
-    primes = sieveOfAtkin(10 ** 6)
-    if n > len(primes):
-        return None
-    return primes[n - 1]
+            yield n
+            for k in range(n * n, limit + 1, 2 * n * n):
+                sieve[k] = False
 
 if __name__ == '__main__':
-    start = time.time()
-    print(nthPrime(10001))
-    print(time.time() - start)
+    sieveOfAtkin(10 ** 8)
+
+""" time complexity: 'O(n / log log n)'; traditionally faster than SOE as more composites are sieved in fewer iterations. """
