10448c934d1009f77e161cf11a90e248a6fb99a8
Liam Pochin <125910815+SonicJuice@users.noreply.github.com>
2024-03-31 14:33:49 +0100

    Update sieve_of_eratosthenes.py

--- Prime-Based Algorithms/sieve_of_eratosthenes.py
+++ Prime-Based Algorithms/sieve_of_eratosthenes.py
@@ -1,13 +1,13 @@
 from math import isqrt
 import numpy as np
 
-def sieveOfEratosthenes(limit):
+def sieve_of_eratosthenes(limit):
     if limit < 2:
         return
 
     """ represents the number of odds <= 'limit'. """
     size = (limit - 1) // 2
-    """ 'np.ones()' returns a new array of the given shape and data type, with values of '1'; acts as a primality tracker. """
+    """ 'np.ones()' returns a new array of the given shape and data type, with values of '1', to track primality. """
     prime = np.ones(size + 1, dtype=bool)
 
     """ '.isqrt()' gets the integer square root of the given non-negative integer 'n'. It returns the floor value of the exact sqrt 
@@ -18,13 +18,12 @@ def sieveOfEratosthenes(limit):
             """ updates all elements of 'prime' that correspond to multiples of the current prime 'p'. """
             prime[i + p::p] = False
 
-    """ '.concatenate()' joins two or more arrays along a specified axis; '.nonzero()' computes the indices of non-zero elements, 
-    returning a tuple of arrays, one for each dimension, containing said indices that dimension."""
+    """ 'np.concatenate()' joins two or more arrays along a specified axis; '.nonzero()' computes the indices of non-zero elements, 
+    returning a tuple of arrays, one for each dimension, containing said indices for that dimension."""
     primes = np.concatenate(([2], np.nonzero(prime)[0] * 2 + 1))
     
     return primes
 
 if __name__ == '__main__':
+    """ time complexity: 'O(n log log n)' """
     sieveOfEratosthenes(10 ** 8)
-
-""" time complexity: 'O(n log log n)' """
