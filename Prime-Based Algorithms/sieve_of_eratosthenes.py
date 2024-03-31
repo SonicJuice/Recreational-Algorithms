e8a346d8f71a761f69eb3f0debe9298ba3deef4d
Liam Pochin <125910815+SonicJuice@users.noreply.github.com>
2024-03-31 14:35:24 +0100

    Update sieve_of_eratosthenes.py

--- Prime-Based Algorithms/sieve_of_eratosthenes.py
+++ Prime-Based Algorithms/sieve_of_eratosthenes.py
@@ -4,12 +4,10 @@ import numpy as np
 def sieve_of_eratosthenes(limit):
     if limit < 2:
         return
-
     """ represents the number of odds <= 'limit'. """
     size = (limit - 1) // 2
     """ 'np.ones()' returns a new array of the given shape and data type, with values of '1', to track primality. """
     prime = np.ones(size + 1, dtype=bool)
-
     """ '.isqrt()' gets the integer square root of the given non-negative integer 'n'. It returns the floor value of the exact sqrt 
     of 'n', or equivalently the greatest integer 'a' such that 'a^2 <= n'. """
     for i in range(1, isqrt(limit) // 2 + 1):
@@ -17,11 +15,9 @@ def sieve_of_eratosthenes(limit):
             p = i * 2 + 1
             """ updates all elements of 'prime' that correspond to multiples of the current prime 'p'. """
             prime[i + p::p] = False
-
     """ 'np.concatenate()' joins two or more arrays along a specified axis; '.nonzero()' computes the indices of non-zero elements, 
     returning a tuple of arrays, one for each dimension, containing said indices for that dimension."""
     primes = np.concatenate(([2], np.nonzero(prime)[0] * 2 + 1))
-    
     return primes
 
 if __name__ == '__main__':
