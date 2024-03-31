00ecbfd1f6e033b218c2bb4552168c2cade260d4
Liam Pochin <125910815+SonicJuice@users.noreply.github.com>
2024-03-31 14:31:37 +0100

    Update sieve_of_atkin.py

--- Prime-Based Algorithms/sieve_of_atkin.py
+++ Prime-Based Algorithms/sieve_of_atkin.py
@@ -1,4 +1,4 @@
-def sieveOfAtkin(limit):
+def sieve_of_atkin(limit):
     if limit < 2:
         return 
 
@@ -11,18 +11,18 @@ def sieveOfAtkin(limit):
     for x in range(1, sqrt_limit):
         x_square = squares[x - 1]
         for y_square in squares:
-            """ a) 'n = (4 * x * x) + (y * y)' has an odd no. of solutions (i.e., there's an odd number of distinct pairs '(x, y)' 
-            that satisfy 'and n % 12 = 1 or n % 12 = 5'). """
+            """ a) 'n = (4 * x * x) + (y * y)' has an odd no. of solutions (i.e. an odd number of distinct pairs '(x, y)') 
+            that satisfy 'and n % 12 = 1 or n % 12 = 5'. """
             n = 4 * x_square + y_square
             if n <= limit and (n % 12 == 1 or n % 12 == 5):
                 sieve[n] = not sieve[n]
 
-            """ b) 'n = (3 * x * x) + (y * y)' has an odd no. of solutions 'and n % 12 = 7'. """
+            """ b) 'n = (3 * x * x) + (y * y)' has an odd no. of solutions that satifsy 'and n % 12 = 7'. """
             n = 3 * x_square + y_square
             if n <= limit and n % 12 == 7:
                 sieve[n] = not sieve[n]
 
-            """ c) 'n = (3 * x * x) - (y * y)' has an odd no. of solutions 'if x > y ** 0.5 and n % 12 = 11'. """
+            """ c) 'n = (3 * x * x) - (y * y)' has an odd no. of solutions that satisfy 'if x > y ** 0.5 and n % 12 = 11'. """
             if x > y_square ** 0.5:
                 n = 3 * x_square - y_square
                 if n <= limit and n % 12 == 11:
@@ -38,6 +38,6 @@ def sieveOfAtkin(limit):
     return primes
 
 if __name__ == '__main__':
-    sieveOfAtkin(10 ** 8)
-
-""" time complexity: 'O(n / log log n)'; traditionally faster than SOE as more composites are sieved in fewer iterations. """
+    """ time complexity: 'O(n / log log n)'; traditionally faster than Eratosthenes
+    as more composites are sieved in fewer iterations. """
+    sieve_of_atkin(10 ** 8)
