d8b39055ab5c3f66c34b14bad11ccd5f3c0ae303
Liam Pochin <125910815+SonicJuice@users.noreply.github.com>
2024-04-02 10:30:33 +0100

    Update polygonals.py

--- number_series/polygonals.py
+++ number_series/polygonals.py
@@ -1,10 +1,17 @@
 def polygonals(sides, limit):
-    result = 1
+    polygonal_numbers = []
     n = 1
-    while result < limit + 1:
-        yield result
+    generate = True
+    while generate:
         """
-        nth-s-agonal number: P(s, n) = ((s - 2) * n^2 - (s - 4) * n) / 2. These represent dots arranged as regular polygons 
-        (plane figures of connected line segments, with equal vertex angles and side lengths)
+        nth-s-agonal number: 'P(s, n) = (n * ((s - 2) * n - (s - 4))) /', where 's' and 'n' are the number of sides and index 
+        of the polygonal number. These represent dots arranged as regular polygons (i.e. plane figures of connected line segments, 
+        with equal vertex angles and side lengths)
         """
-        result += (sides - 2) * n + 1
+        result = (n * ((sides - 2) * n - (sides - 4))) // 2
+        if result > limit + 1:
+            generate = False
+        else:
+            polygonal_numbers.append(result)
+            n += 1
+    return polygonal_numbers
