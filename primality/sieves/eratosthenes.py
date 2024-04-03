52f44e5cbe8af6b8c24ede7db1d1669becac26ab
Liam Pochin <125910815+SonicJuice@users.noreply.github.com>
2024-04-03 21:42:07 +0100

    Update eratosthenes.py

--- primality/sieves/eratosthenes.py
+++ primality/sieves/eratosthenes.py
@@ -2,21 +2,21 @@ import math
 import numpy as np
 
 def eratosthenes(limit):
-    if limit < 2:
-        return
-    """ np.ones() returns a new array of the given data type 
-    with values of 1 to track primality. """  
-    numbers = np.ones(limit - 1, dtype=bool)
-    numbers[:2] = False
-    """ math.isqrt() returns the integer square root of the given non-negative 
-    integer This is the floor value of the exact square root of n (i.e the greatest 
-    integer a such that a^2 <= n. """
-    for i in range(2, math.isqrt(limit) + 1):
-        if numbers[i]:
-            numbers[i*i:limit:i] = False
-    """
-    np.nonzero() returns the indices of non-zero elements.
-    """
-    primes = np.nonzero(numbers)[0]
-    """ Time complexity: 'O(n log log n)' """
-    return primes.tolist()
+  if limit < 2:
+      return
+  """ np.ones() returns a new array of the given data type 
+  with values of 1 to track primality. """  
+  numbers = np.ones((limit - 1) // 2, dtype=bool)
+  numbers[:2] = False
+  """ math.isqrt() returns the integer square root of the given non-negative 
+  integer This is the floor value of the exact square root of n (i.e the greatest 
+  integer a such that a^2 <= n. """
+  sqrt_limit = math.isqrt(limit) + 1
+  for i in range(2, sqrt_limit):
+      if numbers[i]:
+          numbers[i*i:limit:i] = False
+  """
+  np.nonzero() returns the indices of non-zero elements.
+  """
+  primes = np.nonzero(numbers)[0]
+  return primes.tolist()
