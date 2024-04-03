e0a1cddb99987b497664de589240543daa7a939e
Liam Pochin <125910815+SonicJuice@users.noreply.github.com>
2024-04-03 23:40:00 +0100

    Update eratosthenes.py

--- primality/sieves/eratosthenes.py
+++ primality/sieves/eratosthenes.py
@@ -6,12 +6,14 @@ def eratosthenes(limit):
         return
     """ np.ones() returns a new array of the given data type 
     with values of 1 to track primality. """  
-    numbers = np.ones(limit, dtype=bool)
+    size = (limit) // 2
+    numbers = np.ones(size, dtype=bool)
     numbers[:2] = False
+    sqrt_limit = math.isqrt(limit)
     """ math.isqrt() returns the integer square root of the given non-negative 
     integer This is the floor value of the exact square root of n (i.e the greatest 
     integer a such that a^2 <= n. """
-    for i in range(2, math.isqrt(limit)):
+    for i in range(2, sqrt_limit):
         if numbers[i]:
             numbers[i*i:limit:i] = False
     """ np.nonzero() returns the indices of non-zero elements. """
