c4915c9c928274b34042b68b1b4c4cbcaf8ca6a7
Liam Pochin <125910815+SonicJuice@users.noreply.github.com>
2024-04-04 20:35:43 +0100

    Update eratosthenes.py

--- primality/sieves/eratosthenes.py
+++ primality/sieves/eratosthenes.py
@@ -2,23 +2,28 @@ import math
 import numpy as np
 
 def eratosthenes(n):
-  """ limit accounts for odd numbers only, as no primes except 2 are even """
-  limit = math.isqrt((n - 1) // 2) + 1
-  """ np.ones() returns a new array of the given data type 
-  with values of 1 to track primality. """
-  primes = np.ones(n + 1, dtype=bool)
-  """ mark multiples of 2 and 3 as not prime, starting from indices 4 and 9, 
-  respectively, in steps of 2 and 6 """
-  primes[:2] = primes[4::2] = primes[9::6] = False
-  for i in range(5, limit, 6):
-      if primes[i]:
-          primes[i * i :: 2 * i] = False
-
-  """ all primes excluding 2 and 3 can be written in the form 6k +/- 1 """
-  for j in range(7, limit, 6):
-      if primes[j]:
-          """ mark multiples of prime as non-prime """
-          primes[j * j :: 2 * j] = False
-        
-  """ np.nonzero() returns the indices of the non-zero values """
-  return np.flatnonzero(primes)
+    """ reaching the square root of n, where n is the upper limit of the range being 
+    sieved, means that all the composites have already been marked. """
+    limit = math.isqrt(n) + 1
+    """ np.ones() returns a new array of the given data type 
+    with values of 1 to track primality. """
+    primes = np.ones(n + 1, dtype=bool)
+    """ even numbers are marked as composite by moving with a step of 2 from index 4; 
+    multiples of 3 also are, being marked via a step of 6 from index 9. """
+    primes[:2] = primes[4::2] = primes[9::6] = False
+    
+    """ all primes excluding 2 and 3 can be written in the form 6k +/- 1; thus, iterate 
+    through and mark all numbers in these forms. """
+    for i in range(5, limit, 6):
+        if primes[i]:
+            primes[i * i :: 2 * i] = False
+  
+    for j in range(7, limit, 6):
+        """ only marks odd multiples; start from i * i since all multiples < this would
+        have already been marked as composite, and moves in steps of 2i to skip even 
+        multiples, which are already marked as composite). """
+        if primes[j]:
+            primes[j * j :: 2 * j] = False
+  
+    """ np.nonzero() returns the indices of the non-zero values """
+    return np.flatnonzero(primes)
