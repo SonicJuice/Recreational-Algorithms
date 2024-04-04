b71bd80f993cf9bda43da8ba404c9694b5cbf97a
Liam Pochin <125910815+SonicJuice@users.noreply.github.com>
2024-04-04 18:30:31 +0100

    Update eratosthenes.py

--- primality/sieves/eratosthenes.py
+++ primality/sieves/eratosthenes.py
@@ -1,27 +1,23 @@
-import numpy as np
 import math
+import numpy as np
 
-def eratosthenes(limit):
-    if limit < 2:
-        return 
-    even_limit = (limit + 1) // 2
-    """ np.ones() returns a new array of the given data type 
-    with values of 1 to track primality. """
-    numbers = np.ones(even_limit, dtype=bool)
-    numbers[0] = False 
+def eratosthenes(n):
+  limit = math.isqrt(n) + 1
+  """ np.ones() returns a new array of the given data type 
+  with values of 1 to track primality. """
+  primes = np.ones(n + 1, dtype=bool)
+  """ mark multiples of 2 and 3 as not prime, starting from indices 4 and 9, 
+  respectively, in steps of 2 and 6 """
+  primes[:2] = primes[4::2] = primes[9::6] = False
+  for i in range(5, limit, 6):
+      if primes[i]:
+          primes[i * i :: 2 * i] = False
 
-    sqrt_limit = math.isqrt(limit)
-    for i in range(1, (sqrt_limit + 1) // 2):
-        if numbers[i]:
-          """ math.isqrt() returns the integer square root of the given non-
-          negative integer This is the floor value of the exact square root 
-          of n (i.e the greatest integer a such that a^2 <= n. """
-          prime = 2 * i + 1
-          """ multiples of the prime as not prime. """
-          numbers[prime * prime // 2: even_limit: prime] = False
+  """ all primes excluding 2 and 3 can be written in the form 6k +/- 1 """
+  for j in range(7, limit, 6):
+      if primes[j]:
+          """ mark multiples of prime as non-prime """
+          primes[j * j :: 2 * j] = False
 
-    """ convert boolean array into a list of primes via the indices of non-
-    zero elements """
-    primes = [2] + [2 * i + 1 for i in np.nonzero(numbers)[0]]
-    """ time complexity: 'O(n log log n)' """
-    return primes
+  """ np.nonzero() returns the indices of the non-zero values """
+  return np.flatnonzero(primes)
