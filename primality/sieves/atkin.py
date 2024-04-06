dfdc1bdb4606145195d9d16c68db25e03c83911e
Liam Pochin <125910815+SonicJuice@users.noreply.github.com>
2024-04-06 03:19:43 +0100

    Update atkin.py

--- primality/sieves/atkin.py
+++ primality/sieves/atkin.py
@@ -4,46 +4,42 @@ def atkin(limit):
     """ all numbers within the limit are deemed composites, which are sieved in 
     three layers: """
     sieve = [False] * (limit + 1)
-    primes = [2, 3]
-    sqrt_limit = math.isqrt(limit) + 1
-  
-    for x in range(1, sqrt_limit):
-        for y in range(1, sqrt_limit):
-            x_square = x * x
-            y_square = y * y
-  
-            """ a) n = (4 * x * x) + (y * y) has an odd no. of solutions (i.e. an odd
-            number of distinct pairs '(x, y)) that satisfy and n % 12 = 1 or 
-            n % 12 = 5. """
-            n = 4 * x_square + y_square
-            if n <= limit and (n % 12 == 1 or n % 12 == 5):
+    sqrt_limit = int(math.sqrt(limit)) + 1
+    x_squared = [x * x for x in range(1, sqrt_limit)]
+    y_squared = [y * y for y in range(1, sqrt_limit)]
+
+    for x_sq in x_squared:
+        for y_sq in y_squared:
+
+            """ a) if n = 4x^2 + y^2 is odd and n % 60 = 1, 13, 17, 29, 37, 41, 49, or 
+            53). Therefore, these numbers are only prime if the quadratic has an odd number 
+            of solutions and if they are square-free (i.e. not divisible by 
+            any perfect square besides 1)."""
+            n = 4 * x_sq + y_sq
+            if n <= limit and n % 60 in (1, 13, 17, 29, 37, 41, 49, 53):
                 sieve[n] = not sieve[n]
-  
-            """ b) n = (3 * x * x) + (y * y) has an odd no. of solutions 
-            that satifsy and n % 12 = 7. """
-            n = 3 * x_square + y_square
-            if n <= limit and n % 12 == 7:
+
+            """ b) if n = 3x^2 + y^2 is odd and n % 60 = 7, 11, 19, 31, or 37 """
+            n = 3 * x_sq + y_sq
+            if n <= limit and n % 60 in (7, 19, 31, 43):
                 sieve[n] = not sieve[n]
-  
-            """ c) n = (3 * x * x) - (y * y) has an odd no. of solutions that 
-            satisfy if x > y ** 0.5 and n % 12 = 11. """
-            if x > y:
-                n = 3 * x_square - y_square
-                if n <= limit and n % 12 == 11:
+
+            """ c) if n = 3x^2 - y^2 is odd and n % 60 = 11, 23, 47 or 59. """
+            if x_sq > y_sq:
+                n = 3 * x_sq - y_sq
+                if n <= limit and n % 60 in (11, 23, 47, 59):
                     sieve[n] = not sieve[n]
-  
+
     """ mark all multiples of squares as non-prime. """    
     for n in range(5, sqrt_limit, 2):
         if sieve[n]:
-            n_square = n * n
-            for k in range(n_square, limit + 1, n_square):
-                sieve[k] = False
-  
+            n_sq = n * n
+            sieve[n_sq::n_sq] = [False] * ((limit - n_sq) // n_sq + 1)
+
     """ append sieved primes. """
-    for n in range(5, limit + 1, 2):
-        if sieve[n]:
-            primes.append(n)
-  
+    primes = [2, 3, 5]
+    primes.extend([n for n in range(5, limit + 1, 2) if sieve[n]])
+
     """ time complexity: 'O(n / log log n)'; traditionally faster than Eratosthenes
     as more composites are sieved in fewer iterations. """
     return primes
