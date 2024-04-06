7f0010a37e6d428e821f3bf4edba9954f3f5e860
Liam Pochin <125910815+SonicJuice@users.noreply.github.com>
2024-04-06 14:59:10 +0100

    Update atkin.py

--- primality/sieves/atkin.py
+++ primality/sieves/atkin.py
@@ -17,18 +17,18 @@ def atkin(limit):
             of solutions and if they are square-free (i.e. not divisible by 
             any perfect square besides 1)."""
             n = 4 * x_sq + y_sq
-            if n <= limit and n % 60 in (1, 13, 17, 29, 37, 41, 49, 53):
+            if n <= limit and n % 60 in {1, 13, 17, 29, 37, 41, 49, 53}:
                 sieve[n] = not sieve[n]
 
             """ b) if n = 3x^2 + y^2 is odd and n % 60 = 7, 11, 19, 31, or 37 """
             n = 3 * x_sq + y_sq
-            if n <= limit and n % 60 in (7, 19, 31, 43):
+            if n <= limit and n % 60 in {7, 19, 31, 43}:
                 sieve[n] = not sieve[n]
 
             """ c) if n = 3x^2 - y^2 is odd and n % 60 = 11, 23, 47 or 59. """
             if x_sq > y_sq:
                 n = 3 * x_sq - y_sq
-                if n <= limit and n % 60 in (11, 23, 47, 59):
+                if n <= limit and n % 60 in {11, 23, 47, 59}:
                     sieve[n] = not sieve[n]
 
     """ mark all multiples of squares as non-prime. """    
@@ -37,10 +37,6 @@ def atkin(limit):
             n_sq = n * n
             sieve[n_sq::n_sq] = [False] * ((limit - n_sq) // n_sq + 1)
 
-    """ append sieved primes. """
-    primes = [2, 3, 5]
-    primes.extend([n for n in range(5, limit + 1, 2) if sieve[n]])
-
-    """ time complexity: 'O(n / log log n)'; traditionally faster than Eratosthenes
-    as more composites are sieved in fewer iterations. """
-    return primes
+    """ append sieved primes; time complexity: 'O(n / log log n)'. This is traditionally 
+    faster than Eratosthenes as more composites are sieved in fewer iterations. """
+    return np.r_[2, 3, 5, np.nonzero(sieve)[0]]
