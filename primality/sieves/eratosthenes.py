3089ec3b522f56e0b264d2093473ede3cb1de89c
Liam Pochin <125910815+SonicJuice@users.noreply.github.com>
2024-04-06 12:58:45 +0100

    Update eratosthenes.py

--- primality/sieves/eratosthenes.py
+++ primality/sieves/eratosthenes.py
@@ -11,20 +11,20 @@ def eratosthenes(n):
     """ even numbers are marked as composite by moving with a step of 2 from index 4; 
     multiples of 3 also are, being marked via a step of 6 from index 9. """
     primes[:2] = primes[4::2] = primes[9::6] = False
-    
+
     """ all primes excluding 2 and 3 can be written in the form 6k +/- 1; thus, iterate 
     through and mark all numbers in these forms. """
-    for i in range(5, limit, 6):
+    for i in range(5, math.isqrt(limit), 6):
         if primes[i]:
             primes[i * i :: 2 * i] = False
-  
+
     for j in range(7, limit, 6):
         """ only mark odd multiples; start from i * i since all multiples < this would
         have already been marked as composite, and move in steps of 2i to skip even 
         multiples, which are already marked as composite). """
         if primes[j]:
             primes[j * j :: 2 * j] = False
-  
+
     """ np.nonzero() returns the indices of the non-zero values; time complexity: 
     'O(n log log n)'"""
-    return np.flatnonzero(primes)
+    return np.where(primes)[0]
