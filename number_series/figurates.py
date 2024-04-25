a18e7b2fa13a47c9673439437742d0af3c7120a2
Liam Pochin <125910815+SonicJuice@users.noreply.github.com>
2024-04-25 14:30:07 +0100

    Update figurates.py

--- number_series/figurates.py
+++ number_series/figurates.py
@@ -1,5 +1,16 @@
 def figurates(sides, limit):
-    """ P(s, n) = (n * ((s - 2) * n - (s - 4))) / 2, where s and n are the number 
-    of sides and index of the figurate number. This can be represented by a 
-    regular geometrical arrangement of equally spaced points. """
-    return [(n * ((sides - 2) * n - (sides - 4))) // 2 for n in range(limit) if (n * ((sides - 2) * n - (sides - 4))) // 2 <= limit]
+    figurate_numbers = []
+    n = 0
+    generate = True
+    while generate:
+        """ P(s, n) = (n * ((s - 2) * n - (s - 4))) / 2, where s and n are the 
+        number of sides and index of the figurate number. This can be represented 
+        by a regular geometrical arrangement of equally spaced points. """
+        result = (n * ((sides - 2) * n - (sides - 4))) // 2
+        if result > limit + 1:
+            generate = False
+        else:
+            figurate_numbers.append(result)
+        n += 1
+    """ time complexity: 'O(n)' """
+    return figurate_numbers
