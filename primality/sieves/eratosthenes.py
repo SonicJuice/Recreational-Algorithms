cac8d5b2639e6a1b3100d8cdf91ba85c30e35555
Liam Pochin <125910815+SonicJuice@users.noreply.github.com>
2024-04-27 16:49:44 +0100

    Update eratosthenes.py

--- primality/sieves/eratosthenes.py
+++ primality/sieves/eratosthenes.py
@@ -1,29 +1,28 @@
-import math
 import numpy as np
+import math
 
 def eratosthenes(n):
-    """ reaching the square root of n, where n is the upper limit of the range being 
-    sieved, means that all the composites have already been marked. """
-    upper_bound = math.isqrt(n) + 1
     """ np.ones() returns a new array of the given data type 
     with values of 1 to track primality. """
-    primes = np.ones(n + 1, dtype=bool)
-    """ even numbers are marked as composite by moving with a step of 2 from index 4; 
-    multiples of 3 also are, being marked via a step of 6 from index 9. """
+    primes = np.ones(n, dtype=bool)
+    """ reaching isqrt(n) means all composites have already been marked. """
+    upper_bound = math.isqrt(len(primes)) + 1
+    """ even numbers are marked as composite by moving with a step of 2 from index 
+    4; as are multiples of 3, being marked via a step of 6 from index 9. """
     primes[:2] = primes[4::2] = primes[9::6] = False
 
-    """ all primes excluding 2 and 3 can be written in the form 6k +/- 1. """
+    """ all primes besides 2 and 3 can be written as 6k +/- 1. """
     for i in range(5, upper_bound, 6):
         if primes[i]:
             primes[i * i :: 2 * i] = False
 
     for j in range(7, upper_bound, 6):
-        """ only mark odd multiples; start from j * j since all multiples < this would
-        have already been marked as composite, and move in steps of 2j to skip even 
-        multiples, which are already marked as composite). """
         if primes[j]:
+            """ only mark odd multiples; start from j * j since lower multiples 
+            would have already been marked as composite. Move in steps 
+            of 2j to skip even multiples. """
             primes[j * j :: 2 * j] = False
 
-    """ np.where() returns the indices of condition-satisfying elements; time complexity: 
-    'O(n log log n)'"""
-    return np.where(primes)[0]
+    """ np.flatnonzero() returns non-zero indices in the flattened version of a; 
+    time complexity: O(n log log n) """
+    return np.flatnonzero(primes)
