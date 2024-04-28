8ec6332a9afd513fb8c1ee0edd538357536a8655
Liam Pochin <125910815+SonicJuice@users.noreply.github.com>
2024-04-28 11:49:14 +0100

    Update atkin.py

--- primality/sieves/atkin.py
+++ primality/sieves/atkin.py
@@ -8,6 +8,9 @@ def atkin(limit):
     upper_bound = math.isqrt(limit) + 1
     x_squared = [x * x for x in range(1, upper_bound)]
     y_squared = [y * y for y in range(1, upper_bound)]
+    set_1 = {1, 13, 17, 29, 37, 41, 49, 53}
+    set_2 = {7, 19, 31, 43}
+    set_3 = {11, 23, 47, 59}
 
     for x_sq in x_squared:
         for y_sq in y_squared:
@@ -17,18 +20,18 @@ def atkin(limit):
             has an odd number of solutions and if they are square-free (not 
             divisible by any perfect square besides 1). """
             n = 4 * x_sq + y_sq
-            if n <= limit and n % 60 in {1, 13, 17, 29, 37, 41, 49, 53}:
+            if n <= limit and n % 60 in set_1:
                 sieve[n] = not sieve[n]
 
             """ b). if n = 3x^2 + y^2 is odd and n % 60 = 7, 11, 19, 31, or 37 """
             n = 3 * x_sq + y_sq
-            if n <= limit and n % 60 in {7, 19, 31, 43}:
+            if n <= limit and n % 60 in set_2:
                 sieve[n] = not sieve[n]
 
             """ c). if n = 3x^2 - y^2 is odd and n % 60 = 11, 23, 47 or 59. """
             if x_sq > y_sq:
                 n = 3 * x_sq - y_sq
-                if n <= limit and n % 60 in {11, 23, 47, 59}:
+                if n <= limit and n % 60 in set_3:
                     sieve[n] = not sieve[n]
 
     """ mark multiples of squares as non-prime. """    
