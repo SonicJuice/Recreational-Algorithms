58bf19db3ff8cb363afd2533e78651017c223f96
Liam Pochin <125910815+SonicJuice@users.noreply.github.com>
2024-09-29 21:10:52 +0000

    Update miller_rabin.py

--- number_theory/miller_rabin.py
+++ number_theory/miller_rabin.py
@@ -2,11 +2,11 @@ from random import randint
 
 def _modular_exponent(x, y, p):
     result = 1 
-    x %= p # reduce 'x' if it's '>= p' to prevent overflow
+    x %= p # reduce x if >= p to prevent overflow
     while y > 0:
-        if y & 1: # determine if 'y' is odd via bitwise AND
+        if y & 1: # determine if y is odd via bitwise AND
             result = (result * x) % p
-        y >>= 1 # make 'y' even by right shifting by 1
+        y >>= 1 # make y even by right shifting by 1
         x = (x * x) % p
     return result
 
@@ -18,34 +18,25 @@ def miller_rabin(n):
     d = n - 1
     while d % 2 == 0:
         d //= 2
-        """
-        Fermat's little theorem: 'a^n ≡ a % n' for prime 'n', if 'a' is coprime 
-        (i.e. has no common factors w/ 'n' other than 1).
-        """
+        """ Fermat's little theorem: a^n ≡ a % n for prime n, if a is coprime 
+        (i.e. has no common factors w/ n other than 1). """
     for _ in range(4):
-        """
-        from the previous point, it can be seen that for a random base 'a' in the range '[2.. n - 2]', 'a^(d * 2^r) % n' 
-        must be 1 for 'n' to be prime; more random base selections improve accuracy.
-        """
+        """ from the previous point, it can be seen that for a random base a in the range 
+        [2.. n - 2], a^(d * 2^r) % n must be 1 for n to be prime; more random base selections 
+        improve accuracy. """
         a = 2 + randint(1, n - 4) 
-        x = _modular_exponent(a, d, n) # computer 'a^d mod n'
-        """
-        Euclid's lemma: if prime 'n' divides the product of two numbers '(x * y)', it must divide at 
-        least one of those numbers. Thus, '(x^2 % n = 1)' or '(x % n = 1 or x % n = -1)'        
-        """
+        x = _modular_exponent(a, d, n) # computer a^d mod n
+        """ Euclid's lemma: if prime n divides the product of two numbers (x * y), it must divide at 
+        least one of those numbers. Thus, (x^2 % n = 1) or (x % n = 1 or x % n = -1) """
         if x != 1 and x != n - 1: 
             while d != n - 1 and x != n - 1:
                 x = (x * x) % n
                 d *= 2
                 if x == 1:
                     return False
-            """
-            from points 1 and 3, it can be seen that for 'n' to be prime, 'a^d % n = 1' or 'a^(d * 2i) % n = -1' 
-            for some 'i' where '0 <= i <= r-1'.
-            """
+            """ from points 1 and 3, it can be seen that for n to be prime, a^d % n = 1 or 
+            a^(d * 2i) % n = -1 for some i where 0 <= i <= r-1. """
             if x != n - 1:
                 return False
-    """
-    time complexity: 'O(k * logn)'
-    """
+
     return True
