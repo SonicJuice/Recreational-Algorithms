b89c9377a9e67d75c97b10ee921276e22d56b46c
Liam Pochin <125910815+SonicJuice@users.noreply.github.com>
2024-09-30 21:01:09 +0000

    Delete number_types directory

--- number_types/figurates.py
+++
@@ -1,5 +0,0 @@
-def figurates(sides, limit):
-    """ P(s, n) = (n * ((s - 2) * n - (s - 4))) / 2, where s and n are the 
-    number of sides and index of the figurate number. This can be represented 
-    by a regular geometrical arrangement of equally spaced points. """
-    return [figuarate for n in range(1, limit) if (figuarate := n * ((sides - 2) * n - (sides - 4)) // 2) < limit]
